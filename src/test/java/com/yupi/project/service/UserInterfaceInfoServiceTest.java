f43f18d0c48336d64612e41ceb414e91efeb94d9
Hanis <hanisphy@gmail.com>
2024-09-23 18:41:22 -0700

    Save before modifying code using hanisapi-common jar package

--- src/test/java/com/yupi/project/service/UserInterfaceInfoServiceTest.java
+++ src/test/java/com/yupi/project/service/UserInterfaceInfoServiceTest.java
@@ -1,6 +1,5 @@
 package com.yupi.project.service;
 
-import com.hanis.haniscommon.service.UserInterfaceInfoService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
